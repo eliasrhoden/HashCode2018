6aee2639205b1b46ff9d20b1509c58becffb2e37
Elias R <elias.rhoden@hotmail.com>
2018-03-04 17:26:20 +0100

    Testen OK

--- src/RideOptimizer.java
+++ src/RideOptimizer.java
@@ -1,3 +1,5 @@
+import java.sql.Time;
+import java.util.ArrayDeque;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -51,22 +53,118 @@ public class RideOptimizer {
     }
 
     public void bookCar(int car, Ride ride, int startOfRide){
-
+        for(int i =startOfRide;i<startOfRide + ride.stepsRequired;i++){
+            schedules[car][i] = ride;
+        }
     }
 
     public static class CarScheduleAnalyzer{
         private Ride[] schedule;
+        private boolean fits;
+        private int firstStep;
+        private List<TimeBlock> freeTime;
+
 
         public CarScheduleAnalyzer(Ride[] carShedule, Ride r){
+            if(r.END_STEP - r.START_STEP < r.stepsRequired){
+                fits = false;
+                return;
+            }
+            fits = false;
+            this.schedule = carShedule;
+            freeTime = getFreeTime(carShedule);
+            for(TimeBlock tb:freeTime){
+                Position prePos = prevPos(tb.start);
+                Position postPos = postPos(tb.end);
+                int stepsToMoveToStartPos = prePos.stepsTo(r.startPosition);
+                int stepToMoveToNextPos = 0;
+                if(postPos != null){
+                    stepToMoveToNextPos = r.endPosition.stepsTo(postPos);
+                }
+                int earliestBooking = tb.start + stepsToMoveToStartPos;
+                int latestEnd = tb.end - stepToMoveToNextPos;
+                if(earliestBooking <= r.START_STEP && r.START_STEP + r.stepsRequired + stepToMoveToNextPos <= tb.end){
+                    fits = true;
+                    firstStep = r.START_STEP;
+                }
+                if(earliestBooking >= r.START_STEP && earliestBooking + r.stepsRequired - 1 + stepToMoveToNextPos <= tb.end){
+                    fits = true;
+                    firstStep = earliestBooking;
+                }
+            }
+        }
 
+
+        private Position postPos(int end) {
+            for(int i = end;i<schedule.length;i++){
+                if(schedule[i] != null){
+                    return schedule[i].startPosition;
+                }
+            }
+            return null;
+        }
+
+        private Position prevPos(int start) {
+            for(int i = start;i>=0;i--){
+                if(schedule[i] != null){
+                    return schedule[i].endPosition;
+                }
+            }
+            return new Position(0,0);
         }
 
         public boolean rideFitsInschedule(){
-            return false;
+            return fits;
         }
 
         public int getStartStep(){
-            return 0;
+            if(fits)
+                return firstStep;
+            else
+                throw new RuntimeException("Ride doesn't fit in schedule, can't retrieve start step");
+        }
+
+        public static List<TimeBlock> getFreeTime(Ride[] schedule){
+            List<TimeBlock> res = new LinkedList<>();
+            int inARow = 0;
+            int start = 0;
+            for(int i = 0;i<schedule.length;i++){
+                if(schedule[i]== null){
+                    if(inARow == 0){
+                        start = i;
+                    }
+                    inARow++;
+                }else{
+                    if(inARow > 1){
+                        TimeBlock block = new TimeBlock(start, i-1);
+                        res.add(block);
+                    }
+                    inARow = 0;
+                }
+            }
+            if(inARow > 1){
+                TimeBlock block = new TimeBlock(start, schedule.length-1);
+                res.add(block);
+            }
+
+            return res;
+        }
+
+        static class TimeBlock{
+            public int start;
+            public int end;
+            public TimeBlock(int start, int end){
+                this.start = start; this.end = end;
+            }
+
+            @Override
+            public boolean equals(Object o){
+                if(o instanceof TimeBlock){
+                    TimeBlock tb = (TimeBlock) o;
+                    return start == tb.start && end == tb.end;
+                }
+                return false;
+            }
         }
     }
 }
--- tests/RideOptimizerTest.java
+++ tests/RideOptimizerTest.java
@@ -1,4 +1,6 @@
 import org.junit.Test;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class RideOptimizerTest {
@@ -14,18 +16,35 @@ public class RideOptimizerTest {
 
         opt.bookCar(0,r1,2);
 
+        /**
+         * Booked
+         * Step : Booking
+         * s0: null
+         * s1: null
+         * s2: 2
+         * s3: 2
+         * s4: 2
+         * s5: 2
+         * s6: 2
+         * s7: null
+         * s8: null
+         * s9: null
+         * s10: null
+         * */
+
+
         assertEquals(null,opt.schedules[0][0]);
-        assertEquals(null,opt.schedules[0][6]);
+        assertEquals(null,opt.schedules[0][7]);
         assertEquals(null,opt.schedules[1][3]);
 
-        for(int i = 1;i<5;i++){
+        for(int i = 2;i<7;i++){
             Ride br = opt.schedules[0][i];
             assertEquals(r1,br);
         }
     }
 
     @Test
-    public void testAnalyzer(){
+    public void testFitsInSchedule1(){
         Ride[] schedule = new Ride[10];
         Ride r1 = new Ride(
                 1,
@@ -50,7 +69,7 @@ public class RideOptimizerTest {
     }
 
     @Test
-    public void testAnalyzer2(){
+    public void testFitsInSchedule2(){
         Ride[] schedule = new Ride[10];
         Ride r1 = new Ride(
                 1,
@@ -65,9 +84,7 @@ public class RideOptimizerTest {
     }
 
     @Test
-    public void testAnalyzer3(){
-        //TODO
-
+    public void testFitsInSchedule3(){
         Ride[] schedule = new Ride[10];
         Ride r1 = new Ride(
                 1,
@@ -76,25 +93,547 @@ public class RideOptimizerTest {
                 0, 1);
         Ride r2 = new Ride(
                 2,
-                new Position(0,8),
-                new Position(0,5),
-                10, 13);
+                new Position(0,2),
+                new Position(0,0),
+                5, 9);
         Ride r3 = new Ride(
                 3,
+                new Position(0,2),
                 new Position(0,3),
-                new Position(0,8),
                 1, 8);
 
-        for(int i = 0;i<2;i++){
-            schedule[i] = r1;
-        }
+        schedule[0] = r1;
+        schedule[1] = r1;
 
-        RideOptimizer.CarScheduleAnalyzer analyzer = new RideOptimizer.CarScheduleAnalyzer(schedule,r2);
+        schedule[6] = r2;
+        schedule[7] = r2;
+        schedule[8] = r2;
+
+        RideOptimizer.CarScheduleAnalyzer analyzer = new RideOptimizer.CarScheduleAnalyzer(schedule,r3);
 
         assertEquals(true, analyzer.rideFitsInschedule());
         assertEquals(2, analyzer.getStartStep());
+    }
+
+    @Test
+    public void testFitsInSchedule4(){
+        Ride[] schedule = new Ride[10];
+        Ride r1 = new Ride(
+                1,
+                new Position(0,0),
+                new Position(0,2),
+                0, 1);
+        Ride r2 = new Ride(
+                2,
+                new Position(0,7),
+                new Position(0,5),
+                5, 9);
+        Ride r3 = new Ride(
+                3,
+                new Position(0,5),
+                new Position(0,7),
+                1, 8);
+
+        /**
+         * Booked
+         * Step : Col : Booking
+         * s0: 0 : 1
+         * s1: 1 : 1
+         * s2: 2 :
+         * s3:
+         * s4:
+         * s5:
+         * s6:
+         * s7: 7 : 2
+         * s8: 6 : 2
+         * s9: 5
+         * */
+
+        schedule[0] = r1;
+        schedule[1] = r1;
+
+        schedule[7] = r2;
+        schedule[8] = r2;
+
+        RideOptimizer.CarScheduleAnalyzer analyzer = new RideOptimizer.CarScheduleAnalyzer(schedule,r3);
+
+        assertEquals(true, analyzer.rideFitsInschedule());
+        assertEquals(5, analyzer.getStartStep());
+    }
+
+    @Test
+    public void testFitsInSchedule5(){
+        Ride[] schedule = new Ride[10];
+        Ride r1 = new Ride(
+                1,
+                new Position(0,0),
+                new Position(0,2),
+                0, 1);
+        Ride r2 = new Ride(
+                2,
+                new Position(0,7),
+                new Position(0,5),
+                5, 9);
+        Ride r3 = new Ride(
+                3,
+                new Position(0,2),
+                new Position(0,7),
+                1, 8);
+
+        /**
+         * Booked
+         * Step : Col : Booking
+         * s0: 0 : 1
+         * s1: 1 : 1
+         * s2: 2 :
+         * s3:
+         * s4:
+         * s5:
+         * s6:
+         * s7: 7 : 2
+         * s8: 6 : 2
+         * s9: 5
+         * */
+
+        schedule[0] = r1;
+        schedule[1] = r1;
+
+        schedule[7] = r2;
+        schedule[8] = r2;
+
+        RideOptimizer.CarScheduleAnalyzer analyzer = new RideOptimizer.CarScheduleAnalyzer(schedule,r3);
+
+        assertEquals(true, analyzer.rideFitsInschedule());
+        assertEquals(2, analyzer.getStartStep());
+    }
+
+    @Test
+    public void testFitsInSchedule6(){
+        Ride[] schedule = new Ride[10];
+        Ride r1 = new Ride(
+                1,
+                new Position(0,0),
+                new Position(0,2),
+                0, 1);
+        Ride r2 = new Ride(
+                2,
+                new Position(0,7),
+                new Position(0,5),
+                5, 9);
+        Ride r3 = new Ride(
+                3,
+                new Position(0,2),
+                new Position(0,8),
+                1, 8);
+
+        /**
+         * Booked
+         * Step : Col : Booking
+         * s0: 0 : 1
+         * s1: 1 : 1
+         * s2: 2 :
+         * s3:
+         * s4:
+         * s5:
+         * s6:
+         * s7: 7 : 2
+         * s8: 6 : 2
+         * s9: 5
+         * */
+
+        schedule[0] = r1;
+        schedule[1] = r1;
+
+        schedule[7] = r2;
+        schedule[8] = r2;
+
+        RideOptimizer.CarScheduleAnalyzer analyzer = new RideOptimizer.CarScheduleAnalyzer(schedule,r3);
+
+        assertEquals(false, analyzer.rideFitsInschedule());
+    }
+
+    @Test
+    public void testFitsInSchedule7(){
+        Ride[] schedule = new Ride[10];
+        Ride r1 = new Ride(
+                1,
+                new Position(0,0),
+                new Position(0,2),
+                0, 1);
+        Ride r2 = new Ride(
+                2,
+                new Position(0,7),
+                new Position(0,5),
+                5, 9);
+        Ride r3 = new Ride(
+                3,
+                new Position(0,7),
+                new Position(0,2),
+                1, 8);
+
+        /**
+         * Booked
+         * Step : Col : Booking
+         * s0: 0 : 1
+         * s1: 1 : 1
+         * s2: 2 :
+         * s3:
+         * s4:
+         * s5:
+         * s6:
+         * s7: 7 : 2
+         * s8: 6 : 2
+         * s9: 5
+         * */
+
+        schedule[0] = r1;
+        schedule[1] = r1;
+
+        schedule[7] = r2;
+        schedule[8] = r2;
+
+        RideOptimizer.CarScheduleAnalyzer analyzer = new RideOptimizer.CarScheduleAnalyzer(schedule,r3);
+
+        assertEquals(false, analyzer.rideFitsInschedule());
 
     }
 
+    @Test
+    public void testFitsInSchedule8(){
+        Ride[] schedule = new Ride[10];
+        Ride r1 = new Ride(
+                1,
+                new Position(0,0),
+                new Position(0,2),
+                0, 1);
+        Ride r2 = new Ride(
+                2,
+                new Position(0,7),
+                new Position(0,5),
+                5, 9);
+        Ride r3 = new Ride(
+                3,
+                new Position(0,5),
+                new Position(0,7),
+                5, 8);
+
+        /**
+         * Booked
+         * Step : Col : Booking
+         * s0: 0 : 1
+         * s1: 1 : 1
+         * s2: 2 :
+         * s3:
+         * s4:
+         * s5:
+         * s6:
+         * s7: 7 : 2
+         * s8: 6 : 2
+         * s9: 5
+         * */
+
+        schedule[0] = r1;
+        schedule[1] = r1;
+
+        schedule[7] = r2;
+        schedule[8] = r2;
+
+        RideOptimizer.CarScheduleAnalyzer analyzer = new RideOptimizer.CarScheduleAnalyzer(schedule,r3);
+
+        assertEquals(true, analyzer.rideFitsInschedule());
+        assertEquals(5, analyzer.getStartStep());
+    }
+
+    @Test
+    public void testFitsInSchedule9(){
+        Ride[] schedule = new Ride[10];
+        Ride r1 = new Ride(
+                1,
+                new Position(0,0),
+                new Position(0,2),
+                0, 1);
+        Ride r2 = new Ride(
+                2,
+                new Position(0,7),
+                new Position(0,5),
+                5, 9);
+        Ride r3 = new Ride(
+                3,
+                new Position(0,4),
+                new Position(0,5),
+                2, 8);
+
+        /**
+         * Booked
+         * Step : Col : Booking
+         * s0: 0 : 1
+         * s1: 1 : 1
+         * s2: 2 :
+         * s3:
+         * s4:
+         * s5:
+         * s6:
+         * s7: 7 : 2
+         * s8: 6 : 2
+         * s9: 5
+         * */
+
+        schedule[0] = r1;
+        schedule[1] = r1;
+
+        schedule[7] = r2;
+        schedule[8] = r2;
+
+        RideOptimizer.CarScheduleAnalyzer analyzer = new RideOptimizer.CarScheduleAnalyzer(schedule,r3);
+
+        assertEquals(true, analyzer.rideFitsInschedule());
+        assertEquals(4, analyzer.getStartStep());
+    }
+
+    @Test
+    public void testFitsInSchedule10(){
+        Ride[] schedule = new Ride[10];
+        Ride r1 = new Ride(
+                1,
+                new Position(0,0),
+                new Position(0,2),
+                0, 1);
+        Ride r2 = new Ride(
+                2,
+                new Position(0,7),
+                new Position(0,5),
+                5, 9);
+        Ride r3 = new Ride(
+                3,
+                new Position(0,0),
+                new Position(0,5),
+                2, 6);
+
+        /**
+         * Booked
+         * Step : Col : Booking
+         * s0: 0 : 1
+         * s1: 1 : 1
+         * s2: 2 :
+         * s3:
+         * s4:
+         * s5:
+         * s6:
+         * s7: 7 : 2
+         * s8: 6 : 2
+         * s9: 5
+         * */
+
+        schedule[0] = r1;
+        schedule[1] = r1;
+
+        schedule[7] = r2;
+        schedule[8] = r2;
+
+        RideOptimizer.CarScheduleAnalyzer analyzer = new RideOptimizer.CarScheduleAnalyzer(schedule,r3);
+
+        assertEquals(false, analyzer.rideFitsInschedule());
+
+    }
+
+    @Test
+    public void testFitsInSchedule11(){
+        Ride[] schedule = new Ride[10];
+        Ride r1 = new Ride(
+                1,
+                new Position(0,0),
+                new Position(0,2),
+                0, 1);
+        Ride r2 = new Ride(
+                2,
+                new Position(0,7),
+                new Position(0,5),
+                5, 9);
+        Ride r3 = new Ride(
+                3,
+                new Position(0,0),
+                new Position(0,3),
+                0, 4);
+
+        /**
+         * Booked
+         * Step : Col : Booking
+         * s0: 0 : 1
+         * s1: 1 : 1
+         * s2: 2 :
+         * s3:
+         * s4:
+         * s5:
+         * s6:
+         * s7: 7 : 2
+         * s8: 6 : 2
+         * s9: 5
+         * */
+
+        schedule[0] = r1;
+        schedule[1] = r1;
+
+        schedule[7] = r2;
+        schedule[8] = r2;
+
+        RideOptimizer.CarScheduleAnalyzer analyzer = new RideOptimizer.CarScheduleAnalyzer(schedule,r3);
+
+        assertEquals(false, analyzer.rideFitsInschedule());
+
+    }
+
+    @Test
+    public void testGetFreeTime1(){
+        Ride[] schedule = new Ride[10];
+        Ride r1 = new Ride(
+                1,
+                new Position(0,0),
+                new Position(0,2),
+                0, 1);
+        Ride r2 = new Ride(
+                2,
+                new Position(0,7),
+                new Position(0,5),
+                5, 9);
+
+        /**
+         * Booked
+         * Step : Col : Booking
+         * s0: 0 : 1
+         * s1: 1 : 1
+         * s2: 2 :
+         * s3:
+         * s4:
+         * s5:
+         * s6:
+         * s7: 7 : 2
+         * s8: 6 : 2
+         * s9: 5
+         * */
+
+        schedule[0] = r1;
+        schedule[1] = r1;
+
+        schedule[7] = r2;
+        schedule[8] = r2;
+
+
+        RideOptimizer.CarScheduleAnalyzer.TimeBlock b1 = new RideOptimizer.CarScheduleAnalyzer.TimeBlock(2,6);
+
+        List<RideOptimizer.CarScheduleAnalyzer.TimeBlock> exp;
+        exp = List.of(b1);
+        List<RideOptimizer.CarScheduleAnalyzer.TimeBlock> res;
+        res = RideOptimizer.CarScheduleAnalyzer.getFreeTime(schedule);
+
+        assertEquals(exp,res);
+
+    }
+
+
+
+    @Test
+    public void testGetFreeTime2(){
+        Ride[] schedule = new Ride[10];
+        Ride r1 = new Ride(
+                1,
+                new Position(0,0),
+                new Position(0,2),
+                0, 1);
+        Ride r2 = new Ride(
+                2,
+                new Position(0,7),
+                new Position(0,5),
+                5, 9);
+
+        /**
+         * Booked
+         * Step : Col : Booking
+         * s0: 0 : 1
+         * s1: 1 : 1
+         * s2: 2 :
+         * s3:
+         * s4:
+         * s5: : 2
+         * s6: : 2
+         * s7:
+         * s8:
+         * s9:
+         * */
+
+        schedule[0] = r1;
+        schedule[1] = r1;
+
+        schedule[5] = r2;
+        schedule[6] = r2;
+
+
+        RideOptimizer.CarScheduleAnalyzer.TimeBlock b1 = new RideOptimizer.CarScheduleAnalyzer.TimeBlock(2,4);
+        RideOptimizer.CarScheduleAnalyzer.TimeBlock b2 = new RideOptimizer.CarScheduleAnalyzer.TimeBlock(7,9);
+
+        List<RideOptimizer.CarScheduleAnalyzer.TimeBlock> exp;
+        exp = List.of(b1,b2);
+        List<RideOptimizer.CarScheduleAnalyzer.TimeBlock> res;
+        res = RideOptimizer.CarScheduleAnalyzer.getFreeTime(schedule);
+
+        assertEquals(exp,res);
+
+    }
+
+    @Test
+    public void testGetFreeTime3(){
+        Ride[] schedule = new Ride[10];
+
+
+        RideOptimizer.CarScheduleAnalyzer.TimeBlock b1 = new RideOptimizer.CarScheduleAnalyzer.TimeBlock(0,9);
+
+        List<RideOptimizer.CarScheduleAnalyzer.TimeBlock> exp;
+        exp = List.of(b1);
+        List<RideOptimizer.CarScheduleAnalyzer.TimeBlock> res;
+        res = RideOptimizer.CarScheduleAnalyzer.getFreeTime(schedule);
+
+        assertEquals(exp,res);
+
+    }
+
+    @Test
+    public void testGetFreeTime4(){
+        Ride[] schedule = new Ride[10];
+
+        Ride r2 = new Ride(
+                2,
+                new Position(0,7),
+                new Position(0,5),
+                5, 9);
+
+        /**
+         * Booked
+         * Step : Col : Booking
+         * s0:
+         * s1:
+         * s2:
+         * s3:
+         * s4:
+         * s5: : 2
+         * s6: : 2
+         * s7:
+         * s8:
+         * s9:
+         * */
+
+        schedule[5] = r2;
+        schedule[6] = r2;
+
+
+        RideOptimizer.CarScheduleAnalyzer.TimeBlock b1 = new RideOptimizer.CarScheduleAnalyzer.TimeBlock(0,4);
+        RideOptimizer.CarScheduleAnalyzer.TimeBlock b2 = new RideOptimizer.CarScheduleAnalyzer.TimeBlock(7,9);
+
+        List<RideOptimizer.CarScheduleAnalyzer.TimeBlock> exp;
+        exp = List.of(b1,b2);
+        List<RideOptimizer.CarScheduleAnalyzer.TimeBlock> res;
+        res = RideOptimizer.CarScheduleAnalyzer.getFreeTime(schedule);
+
+        assertEquals(exp,res);
+
+    }
 
 }
